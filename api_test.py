b392b1207148e7a329ba768f2054d3f447b01000
Jimish Fotariya <fotariyajimish@gmail.com>
2017-11-18 16:11:14 +0530

    api update

--- api_test.py
+++ api_test.py
@@ -3,16 +3,37 @@ import requests
 import json
 area = "United Kingdom"
 
-headers = {"Referer": "https://wayncheng.github.io/jobba/",'Origin': 'https://wayncheng.github.io', "X-Requested-With*": "XMLHttpRequest"}	
-keywordList = ["speaker", "interpreter", "singer", "food tester", "Design Thinker"]
-for text in keywordList:
-	# url = "https://cors-anywhere.herokuapp.com/http://service.dice.com/api/rest/jobsearch/v1/simple.json?text="+text+"&city="+area
-	url = "https://duunitori.fi/api/v1/5d3fc27dec93f5e5105e3443edfc421bb57c3603/jobentries?search="+text+"&format=json&city="+area
-	# url = "https://cors-anywhere.herokuapp.com/http://service.dice.com/api/rest/jobsearch/v1/simple.json?text="+text+"&city="+area
-	
-	r = requests.get(url,headers=headers	)
-	print r.text
-	# tempJson = json.loads(r.text)
-	# print tempJson
 
+
+def get5( ar ):
+	dataList = [] 
+	i = 0
+	for x in ar:
+		i+=1 
+		dataList.append( results )
+		if ( i == 5 ):
+			break
+		pass
+	return dataList
+
+keywordList = ["speaker", "interpreter", "singer", "food tester", "Design Thinker"]
+
+for text in keywordList:
+	url = "https://duunitori.fi/api/v1/5d3fc27dec93f5e5105e3443edfc421bb57c3603/jobentries?search="+text+"&format=json&city="+area
+	r = requests.get(url)
+	tempJson = json.loads(r.text)
+	dataList1 = get5(tempJson['results'])
+
+	headers = {"Referer": "https://wayncheng.github.io/jobba/",'Origin': 'https://wayncheng.github.io', "X-Requested-With*": "XMLHttpRequest"}		
+	url = "https://cors-anywhere.herokuapp.com/http://service.dice.com/api/rest/jobsearch/v1/simple.json?text="+text+"&city="+area
+	r = requests.get(url,headers=headers	)
+	tempJson = json.loads(r.text)
+	dataList2 = get5(tempJson['results'])
+	
+
+	print dataList1
+	print " ---- "
+	print dataList2
+	
+	break;
 	pass
