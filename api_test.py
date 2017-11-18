54b5501bb722b71d611f6da895af721e509aafc0
Jimish Fotariya <fotariyajimish@gmail.com>
2017-11-18 16:40:28 +0530

    results update

--- api_test.py
+++ api_test.py
@@ -8,7 +8,7 @@ def get5( ar ):
 	i = 0
 	for x in ar:
 		i+=1 
-		dataList.append( results )
+		dataList.append( x )
 		if ( i == 5 ):
 			break
 		pass
@@ -22,7 +22,17 @@ for text in keywordList:
 	url = "https://duunitori.fi/api/v1/5d3fc27dec93f5e5105e3443edfc421bb57c3603/jobentries?search="+text+"&format=json&city="+area
 	r = requests.get(url)
 	tempJson = json.loads(r.text)
-	jobs.append( get5(tempJson['results']) )
+	if "results" in  tempJson:
+		jobs.append( get5(tempJson['results']) )
+
+	headers = {"Referer": "https://wayncheng.github.io/jobba/",'Origin': 'https://wayncheng.github.io', "X-Requested-With*": "XMLHttpRequest"}		
+	url = "https://cors.now.sh/http://api.indeed.com/ads/apisearch?publisher=422492215893931&userip=1.2.3.4&useragent=Chrome&v=2&format=json&limit=50&q="+text+"&city="+area
+	r = requests.get(url,headers=headers	)
+	tempJson = json.loads(r.text)
+
+	if "results" in  tempJson:
+		jobs.append( get5(tempJson['results']) )
+
 	pass
 
 shuffle(jobs)
